8415ee3425d15b8ec982e5341e1cbc84f5d0c6a3
Julian Baccin <julibaccin01@gmail.com>
2021-09-24 13:02:29 -0300

    detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './pages/detail/detail.component';
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'detalle/:id', component: DetailComponent, pathMatch: 'full' },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { RegisterComponent } from './pages/register/register.component';
 import { PanelComponent } from './pages/panel/panel.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HomeComponent, LoginComponent, RegisterComponent, PanelComponent, FooterComponent, HeaderComponent],
+  declarations: [AppComponent, HomeComponent, HomeComponent, LoginComponent, RegisterComponent, PanelComponent, FooterComponent, HeaderComponent, DetailComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
