459ae0ad51069754de3ae18fc42b7b3f1335f975
Julian Baccin <julibaccin01@gmail.com>
2021-10-07 18:08:17 -0300

    fix: enviroments

--- README.md
+++ README.md
@@ -7,5 +7,14 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 Cuenta de firebase
 
 -STORAGE
+
+Tener 2 imagenes
+user.svg
+house.svg
+
 -FIREBASEAUTH
 -FIRESTORE
+
+# DEPLOY
+
+ng deploy --base-href=https://julibaccin.github.io/venadopropiedades/
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,15 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-  apiKey: "AIzaSyCveTZsY-M0f-n2o7KPI2z2mdSs03DU23Y",
-  authDomain: "venadopropiedades.firebaseapp.com",
-  projectId: "venadopropiedades",
-  storageBucket: "venadopropiedades.appspot.com",
-  messagingSenderId: "776134353786",
-  appId: "1:776134353786:web:d48d3dd3693935a61c6cc3",
-  measurementId: "G-DDRH3CNXMG",
+  firebaseConfig: {
+    apiKey: 'AIzaSyCveTZsY-M0f-n2o7KPI2z2mdSs03DU23Y',
+    authDomain: 'venadopropiedades.firebaseapp.com',
+    projectId: 'venadopropiedades',
+    storageBucket: 'venadopropiedades.appspot.com',
+    messagingSenderId: '776134353786',
+    appId: '1:776134353786:web:d48d3dd3693935a61c6cc3',
+    measurementId: 'G-DDRH3CNXMG',
   },
   // URL GENERICAS
-  urlHouse: 'https://firebasestorage.googleapis.com/v0/b/venadopropiedades.appspot.com/o/images%2Fhouse.svg?alt=media&token=11da2d4f-8fdb-483b-9905-b6664644f000';
+  urlHouse:
+    'https://firebasestorage.googleapis.com/v0/b/venadopropiedades.appspot.com/o/images%2Fhouse.svg?alt=media&token=11da2d4f-8fdb-483b-9905-b6664644f000',
 };
